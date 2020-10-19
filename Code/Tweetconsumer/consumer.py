1b0a8989c01265b55094c64901acb13f9c98be95
André Godinho <andregodinho06@gmail.com>
2020-10-19 23:06:43 +0100

    100 records

--- Code/Tweetconsumer/consumer.py
+++ Code/Tweetconsumer/consumer.py
@@ -30,7 +30,7 @@ AUTO_OFFSET_RESET = 'earliest'
 ENABLE_AUTO_COMMIT = False
 
 ## maximum number of messages to return
-MAX_POLL_RECORDS = 16 # TODO: how many records to poll?
+MAX_POLL_RECORDS = 100 # TODO: how many records to poll?
 
 ## maximum time to block waiting for message (in seconds)
 MAX_BLOCK_WAIT_TIME = 2
