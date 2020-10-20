16d1ca05f720c527b4250d5d79ad536f6b8d41dd
André Godinho <andregodinho06@gmail.com>
2020-10-20 13:56:22 +0100

    DEBUG

--- Code/BERTSentimentAnalysis/SentimentClassifier/BERT.py
+++ Code/BERTSentimentAnalysis/SentimentClassifier/BERT.py
@@ -13,11 +13,12 @@ class BERT(nn.Module):
         self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
 
     def forward(self, input_ids, attention_mask):
-        print(input_ids)
+        print(self.bert)
 
         _, pooled_output = self.bert(
             input_ids=input_ids, # indices of input sequence tokens in the vocabulary
             attention_mask=attention_mask # mask to avoid performing attention on padding token indices
         )
+        print("POOLED!")
         output = self.dropout(pooled_output)
         return self.out(output)
