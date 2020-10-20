7bb1b24b87677d425ed879f26bfd1d1638703f85
André Godinho <andregodinho06@gmail.com>
2020-10-20 14:18:36 +0100

    debug

--- Code/Tweetconsumer/concurrentconsumer.py
+++ Code/Tweetconsumer/concurrentconsumer.py
@@ -192,7 +192,7 @@ def _process_batch(sentimentclassifier, q, c):
     # batch needs to be in np.ndarray format for batches of dataloader
     batch = np.array(list(batch.values()))
     
-    time.sleep(2)
+    time.sleep(2000)
 
     start = time.process_time()
     predictions = sentimentclassifier.predict(batch, BATCH_SIZE)
