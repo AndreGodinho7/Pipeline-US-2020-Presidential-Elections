8921eba9be676751fde5bd469065a09db83f0cc5
André Godinho <andregodinho06@gmail.com>
2020-10-30 10:56:32 +0000

    write to ES, preprocess of text, retweets, quotes

--- Code/Tweetconsumer/concurrentconsumer.py
+++ Code/Tweetconsumer/concurrentconsumer.py
@@ -3,10 +3,14 @@ import json
 import time
 import sys
 import os
+import gc
 sys.path.append("..") # to be able to import modules
 
 
-
+######## PREPROCESS CONFIGS ########
+import re
+from datetime import datetime
+import emoji
 
 ######## LOGGING CONFIGS ########
 import logging
@@ -38,9 +42,10 @@ MAX_POLL_INTERVAL_MS = POLL_INTERVAL_MIN*60*1000 # TODO: how many milliseconds t
 ######## MODEL CONFIGS ########
 import numpy as np
 import torch
+from torch.utils.data import DataLoader
 from transformers import BertTokenizer, DistilBertTokenizer
 from BERTSentimentAnalysis.DataPreProcess.BERTFormatDataloader import BERTFormatDataloader
-from BERTSentimentAnalysis.DataPreProcess.BERTInferenceDataset import BERTInferenceDataset
+from BERTSentimentAnalysis.DataPreProcess.TweetsDataset import TweetsDataset
 # from BERTSentimentAnalysis.SentimentClassifier.BERTSentimentClassifier import BERTSentimentClassifier
 # from BERTSentimentAnalysis.SentimentClassifier.BERT import BERT
 from BERTSentimentAnalysis.SentimentClassifier.newBERTSentimentClassifier import BERTSentimentClassifier
@@ -61,13 +66,17 @@ torch.manual_seed(RANDOM_SEED)
 #MODEL_NAME = '/home/andregodinho06/Projects/Twitter Project/bert_classifier.bin'
 #MODEL_NAME = '/home/andregodinho06/Projects/Twitter Project/distillbert_classifier.bin'
 
+######## ELASTICSEARCH CONFIGS ########
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+
 
 ######## WORKER CONFIGS ########
 import threading
 from multiprocessing import Process, Barrier
 from queue import Queue
 
-NUM_WORKERS = 64
+NUM_WORKERS = 1
 NUM_THREADS = 1
 barrier = Barrier(NUM_WORKERS)
 
@@ -83,20 +92,88 @@ def create_kafka_config(jsonData):
 
     return conf
 
-def extract_twitter_id_text(record):
+def extract_tweet_info(record):
     record_json = json.loads(record)
+    record_json_keys = list(record_json.keys())
+    print("\n\n\n")
 
-    if 'extended_tweet' in record_json.keys():
-        return record_json['id_str'], record_json['extended_tweet']['full_text']
+    for key, value in record_json.items():
+        print("key: %s | value: %s" %(key, value))
+
+    if record_json['is_quote_status']: # quotes are usually against the user tweet message, do not use neither if it has a retweet
+        if 'extended_tweet' in record_json_keys:
+            text = record_json['extended_tweet']['full_text']
+        else:
+            text = record_json['text']
+    else:
+        if 'retweeted_status' in record_json_keys: # users usually agree with retweet opinions, forward it as tweet message
+            retweet_json_keys = list(record_json['retweeted_status'].keys())
+
+            if 'extended_tweet' in retweet_json_keys:
+                text = record_json['retweeted_status']['extended_tweet']['full_text']
+            else:
+                text = record_json['retweeted_status']['text']
+        
+        else: # normal tweet (without retweet and quote)
+            if 'extended_tweet' in record_json_keys:
+                text = record_json['extended_tweet']['full_text']
+            else:
+                text = record_json['text']
+
+    tweet_id = record_json['id'] 
+    # date example Tue Oct 20 18:54:06 +0000 2020
+    tweet_date = datetime.strptime(record_json['created_at'], '%a %b %d %H:%M:%S %z %Y').isoformat()
+    user_id = record_json['user']['id']
+    user_name = record_json['user']['screen_name']
+    user_location = record_json['user']['location']
+    user_followers = record_json['user']['followers_count']
+    user_friends = record_json['user']['friends_count']
+    user_verified = record_json['user']['verified']
+    user_created_at = datetime.strptime(record_json['user']['created_at'], '%a %b %d %H:%M:%S %z %Y').isoformat()
     
-    return record_json['id_str'], record_json['text']
+    return {
+        tweet_id: {
+            "tweet": text,
+            "sentiment": '',
+            "tweet_created_at": tweet_date,
+            "user_id": user_id,
+            "user_name": user_name,
+            "user_location": user_location,
+            "user_followers": user_followers,
+            "user_friends": user_friends,
+            "user_verified": user_verified,
+            "user_created_at": user_created_at
+        }
+    }
+
+def pre_process_tweet(text):
+    # remove mentions (e.g., @realDonaldTrump) 
+    # remove URLs (e.g., https://t.co/Oh2IsZ6JAw)
+    text = re.sub(r"(?:\@|https?\://)\S+", "", text)
+    
+    # remove 'RT' 
+    text = text.replace("RT", "")
+
+    # remove '#'
+    text = text.replace("#", "")
+
+    # convert emoji to text
+    text = emoji.demojize(text, delimiters=("", ""))
+
+    # replace '_' by ' ' (created by emojis)
+    text = text.replace("_", " ")
+
+    return text
 
 def batch_tweets_dict(records):
-    batch_dict = {}
+    trump_tweets = []
+    biden_tweets = []
+    trump_biden_tweets = []
+
     for record in records:
         if record.error():
             logging.error(
-                'CONSUME (batch_tweets_dict):# %s - Consumer ERROR: %s', 
+                'CONSUME (batch_tweets_dict): # %s - Consumer ERROR: %s', 
                 os.getpid(), record.error()
             )
             continue
@@ -104,25 +181,78 @@ def batch_tweets_dict(records):
         # convert bytes to str
         record_str = record.value().decode('utf-8') 
         try: 
-            id_tweet, text_tweet = extract_twitter_id_text(record_str)
+            tweet_info = extract_tweet_info(record_str)
 
-        except KeyError: 
+        except KeyError as e: 
             logging.info(
-            'CONSUME (batch_tweets_dict):#%s - skipping bad data: %s...', 
-            os.getpid(), record_str
+            'CONSUME (batch_tweets_dict): #%s - Skipping bad data: %s\n%s', 
+            os.getpid(), record_str, str(e)
             )
             continue
 
         except json.decoder.JSONDecodeError: 
             logging.info(
-                'CONSUME (batch_tweets_dict):#%s - skipping bad data: %s...', 
+                'CONSUME (batch_tweets_dict): #%s - JSON Decode error (bad data): %s', 
                 os.getpid(), record_str
             )            
             continue
-
-        batch_dict.update({id_tweet:text_tweet})
         
-    return batch_dict
+        flag_trump = False
+        flag_biden = False
+        tweet = tweet_info[next(iter(tweet_info))].get('tweet')
+
+        if re.search('trump', tweet, re.IGNORECASE):
+            flag_trump = True
+
+        if re.search('biden', tweet, re.IGNORECASE):
+            flag_biden = True
+
+        if flag_trump or flag_biden:
+            tweet = pre_process_tweet(tweet)
+
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FINAL PRE-PROCESSED TWEET: %s\n" %(tweet))
+        
+        if flag_trump and flag_biden:
+            trump_biden_tweets.append(tweet_info)
+
+        elif flag_trump:
+            trump_tweets.append(tweet_info)
+
+        elif flag_biden:
+            biden_tweets.append(tweet_info)
+
+        else:
+            continue
+
+    logging.info(
+        'CONSUME (batch_tweets_dict): #%s - Found - Trump tweets: %d; Biden tweets: %d; Trump & Biden tweets: %d',
+         os.getpid(), len(trump_tweets), len(biden_tweets), len(trump_biden_tweets)
+    )
+
+    return {
+        "trump": trump_tweets,
+        "biden": biden_tweets,
+        "trump_biden":  trump_biden_tweets
+    }
+
+def bulk_tweets(index, candidate_tweets):
+    for tweet_info in candidate_tweets:
+        id = next(iter(tweet_info))
+
+        yield {
+            "_index": '2020elections-'+index,
+            "_id": id,
+            "@timestamp": tweet_info.get(id).get("tweet_created_at"),
+            "sentiment": tweet_info.get(id).get("sentiment"),
+            "tweet": tweet_info.get(id).get("tweet"),
+            "user_id": tweet_info.get(id).get("user_id"),
+            "user_name": tweet_info.get(id).get("user_name"),
+            "user_location": tweet_info.get(id).get("user_location"),
+            "user_followers": tweet_info.get(id).get("user_followers"),
+            "user_friends": tweet_info.get(id).get("user_friends"),
+            "user_verified": tweet_info.get(id).get("user_verified"),
+            "user_created_at": tweet_info.get(id).get("user_created_at")
+        }
 
 def _init_sentiment_classifier(model_name, model_path):
     if model_name == 'bert':
@@ -230,13 +360,9 @@ def _consume(config, model, model_path):
     )
     c = Consumer(**config['kafka_kwargs'])
     c.subscribe([config['topic']])
-    # q = Queue(maxsize=config['num_threads'])
 
     sentimentclassifier = _init_sentiment_classifier(model, model_path)
     barrier.wait()
-    total_time = round(time.process_time(), 5)
-
-    print(total_time)
 
     while True:
         logging.info(
@@ -256,38 +382,58 @@ def _consume(config, model, model_path):
                 time.sleep(2)
                 continue
             
-            # get batch of tweets in a dict {tweet ID: tweet text} (able to get long tweets)
-            batch_records = batch_tweets_dict(records)
-
+            # get batch of tweets (dict of tweets for trump, biden and both)
+            batch_tweets = batch_tweets_dict(records)
             
-            # batch needs to be in np.ndarray format for batches of dataloader
-            start = time.process_time()
-            predictions = sentimentclassifier.predict(np.array(list(batch_records.values())), BATCH_SIZE)
-            total_time = round(time.process_time() - start, 2)
+            for index, candidate_tweets in batch_tweets.items():
+                ids = [next(iter(tweet)) for tweet in candidate_tweets]
 
-            logging.info(
-                'CONSUME: #%s - classification time = %f',
-                os.getpid(), total_time
-            )
+                tweets = [tweet[next(iter(tweet))].get('tweet') for tweet in candidate_tweets]
 
-            # q.task_done()
+                print(ids)
+                print("\n")
+                print(tweets)
 
-            # try:
-            #     c.commit()
+                tweets_dataset = TweetsDataset(ids, tweets)
+                tweets_dataloader = DataLoader(tweets_dataset, batch_size=BATCH_SIZE)
+                
+                # TODO: test GC
+                # gc.collect()
 
-            # except Exception as e:
-            #     logging.critical(
-            #         'CONSUME: #%s THREAD#%s - Exception when committing offsets: %s', 
-            #         os.getpid(), threading.get_ident(), str(e)
-            #     ) 
+                ids, predictions = sentimentclassifier.predict(tweets_dataloader)
 
-            # q.put(batch_records)
+                for id, sentiment in zip(ids, predictions):
+                    candidate_tweets.get(id)['sentiment'] = sentiment
 
-            # # Use default daemon=False to stop threads gracefully in order to
-            # # release resources properly.
-            # t = threading.Thread(target=_process_batch, args=(sentimentclassifier, q, c))
-            # t.start()
-        
+                # feed tweets to ElasticSearch
+                try:
+                    # no memory allocation when sending bulk of tweets to ES
+                    response = helpers.bulk(es, bulk_tweets(index, candidate_tweets))
+
+                    logging.info(
+                        'CONSUME (to ElasticSearch): #%s - %s',
+                        os.getpid(), str(response)
+                    )
+                except Exception as e:
+                    logging.critical(
+                        'CONSUME (to ElasticSearch EXCEPTION): #%s - %s', 
+                        os.getpid(), str(e)
+                    )
+
+            try:
+                c.commit()
+                logging.info(
+                    'CONSUME: #%s - Offsets have ben committed.',
+                    os.getpid()
+                )
+
+            except Exception as e:
+                logging.critical(
+                    'CONSUME: #%s - Exception when committing offsets: %s', 
+                    os.getpid(), str(e)
+                ) 
+            
+
         except KeyboardInterrupt: 
             logging.warning(
                 'CONSUME: #%s - Worker is closing. Closing Kafka consumer gracefully...',
@@ -298,7 +444,7 @@ def _consume(config, model, model_path):
                 'CONSUME: #%s - Kafka consumer closed gracefully.',
                 os.getpid()
             )
-            exit(0)
+            sys.exit(0)
 
         except KafkaError as e: 
             logging.critical(
@@ -332,11 +478,12 @@ def main():
     workers = []
     while True:
         num_alive = len([w for w in workers if w.is_alive()])
-
+        
         # TODO: check CPU usage
         if config['num_workers'] == num_alive:
             continue
-
+        
+        # TODO: in case of failure it connects two other processes
         for _ in range(config['num_workers']-num_alive):
             p = Process(target=_consume, daemon=True, args=(config, model, model_path))
             p.start()
@@ -348,4 +495,5 @@ def main():
 
 
 if __name__ == "__main__":
+    es = Elasticsearch(hosts="localhost", maxsize=NUM_WORKERS)
     main()
