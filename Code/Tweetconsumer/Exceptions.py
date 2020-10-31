c336e32af32a3711b83bdbaf2c8d29963b6e7d57
André Godinho <andregodinho06@gmail.com>
2020-10-31 17:22:15 +0000

    add notenglishtweet exception

--- Code/Tweetconsumer/Exceptions.py
+++ Code/Tweetconsumer/Exceptions.py
@@ -5,3 +5,13 @@ class Error(Exception):
 class InvalidTweet(Error):
     """Raised when the tweet is invalid by not having the defined keys by the official Twitter API"""
     pass
+
+class NotEnglishTweet(Error):
+    "Raised when the tweet is not in English ('lang' parameter of JSON is not 'en')"
+    # Constructor or Initializer  
+    def __init__(self, value):  
+        self.value = value  
+    
+    # __str__ is to print() the value  
+    def __str__(self):  
+        return(repr(self.value))  
