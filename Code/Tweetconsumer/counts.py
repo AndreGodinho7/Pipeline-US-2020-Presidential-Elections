5f4482b9d58eb6569b60b323f129c84a1c7dc406
André Godinho <andregodinho06@gmail.com>
2021-02-18 14:05:52 +0000

    catch

--- Code/Tweetconsumer/counts.py
+++ Code/Tweetconsumer/counts.py
@@ -5,7 +5,9 @@ total = 0
 
 for filename in os.listdir(os.getcwd()):
     with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
-        print(filename)
-        total += int(f.read())
+        try:
+            total += int(f.read())
+        except :
+            print(filename)
 
 print(total)
