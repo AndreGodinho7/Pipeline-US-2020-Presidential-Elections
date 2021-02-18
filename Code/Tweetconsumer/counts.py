352a53efe20b3e429f39f9ea98a9d5e5a458b0c6
André Godinho <andregodinho06@gmail.com>
2021-02-18 14:03:39 +0000

    counts

--- Code/Tweetconsumer/counts.py
+++ Code/Tweetconsumer/counts.py
@@ -3,12 +3,6 @@ workers=64
 
 total = 0
 
-    # try:
-    #     with open('./output'+str(workers)+'/partitions_'+str(part)+'.txt', "r") as f:
-            
-    # except:
-    #     print("Error opening file", part)
-
 for filename in os.listdir(os.getcwd()):
     with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
         total += int(f.read())
